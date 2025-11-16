ee69b3fceba0d301eff05b3d25d4ce4091eaab21
WARATECS <netuimanetuima@gmail.com>
2025-11-16 14:38:30 +1000

    Add Files #12

---
+++ Django-Project-3/simple_signup/sign/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import BaseRegisterView, upgrade_me
+from django.views.decorators.cache import cache_page
+
+urlpatterns = [
+    path('login/',cache_page(10)(LoginView.as_view(template_name = 'sign/login.html')),name='login'),
+    path('logout/',LogoutView.as_view(template_name = 'sign/logout.html'),name='logout'),
+    path('signup/',BaseRegisterView.as_view(template_name = 'sign/signup.html'),name='signup'),
+    path('upgrade/', upgrade_me, name = 'upgrade')
+]
